aad0f6a069b609af3347b1f9f71a2c9632f2a4f2
Apfirebolt <aspper20@gmail.com>
2024-05-01 11:40:41 +0530

    added marketing theme

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    // Code to be executed when the document is ready
+    console.log("Hello, World!");
+
+    // animate div with class hero
+    // $(".hero").animate({
+    //     opacity: 0.25,
+    //     left: "+=50",
+    //     height: "toggle"
+    // }, 5000, function() {
+    //     // Animation complete
+    // });
+});
