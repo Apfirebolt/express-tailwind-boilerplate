0780a65dc0292d42547485098094753978487ab0
Apfirebolt <aspper20@gmail.com>
2024-05-01 12:04:19 +0530

    added mobile menu plus jquery

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,20 @@ $(document).ready(function() {
     // Code to be executed when the document is ready
     console.log("Hello, World!");
 
+    
+    $('#closeMenuButton').click(function() {
+        $('#mobileMenu').fadeOut(500, function() {
+            $(this).removeClass("opacity-100 scale-100").addClass("opacity-0 scale-95");
+        });
+    });
+
+    $('#openMenuButton').click(function() {
+        console.log("Open Menu Button Clicked");
+        $('#mobileMenu').fadeIn(500, function() {
+            $(this).removeClass("opacity-0 scale-95").addClass("opacity-100 scale-100");
+        });
+    });
+
     // animate div with class hero
     // $(".hero").animate({
     //     opacity: 0.25,
